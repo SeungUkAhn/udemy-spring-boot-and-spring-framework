d7b21e81f2ac66930831644a50a3d634f8bd20c7
SeungUk Ahn <ahn2802@gmail.com>
2023-06-29 00:39:04 +0900

    AppGamingBasic - loosely coupled1: 인터페이스

--- src/main/java/com/maicoding/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/maicoding/learnspringframework/AppGamingBasicJava.java
@@ -2,12 +2,16 @@ package com.maicoding.learnspringframework;
 
 import com.maicoding.learnspringframework.game.GameRunner;
 import com.maicoding.learnspringframework.game.MarioGame;
+import com.maicoding.learnspringframework.game.PacmanGame;
+import com.maicoding.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
     public static void main(String[] args) {
-        var marioGame = new MarioGame();
-        var gameRunner = new GameRunner(marioGame);
+        //var game = new MarioGame();
+        //var game = new SuperContraGame();
+        var game = new PacmanGame();
+        var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
 }
--- src/main/java/com/maicoding/learnspringframework/game/GameRunner.java
+++ src/main/java/com/maicoding/learnspringframework/game/GameRunner.java
@@ -1,9 +1,9 @@
 package com.maicoding.learnspringframework.game;
 
 public class GameRunner {
-    MarioGame game;
-    public GameRunner(MarioGame marioGame) {
-        this.game = marioGame;
+    private GamingConsole game;
+    public GameRunner(GamingConsole game) {
+        this.game = game;
     }
 
     public void run() {
