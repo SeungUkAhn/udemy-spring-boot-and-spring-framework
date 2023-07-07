d3ebc7b701c44163b037bd68a5cb5f565f47c6a4
SeungUk Ahn <ahn2802@gmail.com>
2023-07-08 04:47:31 +0900

    Spring Data JPA - 커스텀 메서드 정의와 사용

--- src/main/java/com/maicoding/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/maicoding/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -16,11 +16,20 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception{
-        repository.save(new Course(1, "Learn AWS Now!", "maicoding"));
-        repository.save(new Course(2, "Learn Azure Now!", "maicoding"));
-        repository.save(new Course(3, "Learn DevOps Now!", "maicoding"));
+        repository.save(new Course(1, "Learn AWS Jpa!", "maicoding"));
+        repository.save(new Course(2, "Learn Azure Jpa!", "maicoding"));
+        repository.save(new Course(3, "Learn DevOps Jpa!", "maicoding"));
         repository.deleteById(1l);
         System.out.println(repository.findById(2l));
         System.out.println(repository.findById(3l));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("maicoding"));
+        System.out.println(repository.findByAuthor(""));
+
+        System.out.println(repository.findByName("Learn AWS Jpa!"));
+        System.out.println(repository.findByName("Learn DevOps Jpa!"));
     }
 }
