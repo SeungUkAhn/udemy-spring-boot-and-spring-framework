2452be79e9f6cd3128a20742b85786f89c8d59dd
SeungUk Ahn <ahn2802@gmail.com>
2023-07-10 03:55:34 +0900

    GET /users/{id} 생성

--- src/main/java/com/maicoding/rest/webservices/restfulwsebservices/user/UserResource.java
+++ src/main/java/com/maicoding/rest/webservices/restfulwsebservices/user/UserResource.java
@@ -1,6 +1,7 @@
 package com.maicoding.rest.webservices.restfulwsebservices.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +17,10 @@ public class UserResource {
     @GetMapping(path = "/users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retrieveAllUsers(@PathVariable int id){
+        return service.findOne(id);
     }
 }
