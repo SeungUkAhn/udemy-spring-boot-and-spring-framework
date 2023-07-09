f82750595939f0e13109d541562360735ae02417
SeungUk Ahn <ahn2802@gmail.com>
2023-07-10 03:18:27 +0900

    프로젝트 세팅, HelloWorld 컨트롤러 생성

---
+++ src/main/java/com/maicoding/rest/webservices/restfulwsebservices/RestfulWsebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.maicoding.rest.webservices.restfulwsebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWsebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWsebServicesApplication.class, args);
+    }
+
+}
