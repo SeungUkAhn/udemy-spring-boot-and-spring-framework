aa73cf43dba0c3af1ac82ca427366f2fc152ec87
SeungUk Ahn <ahn2802@gmail.com>
2023-07-10 03:35:58 +0900

    path variable 추가

--- src/main/java/com/maicoding/rest/webservices/restfulwsebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/maicoding/rest/webservices/restfulwsebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.maicoding.rest.webservices.restfulwsebservices.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
@@ -12,8 +9,8 @@ public class HelloWorldController {
         return "HelloWorld";
     }
 
-    @GetMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("HelloWorld");
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean("Hello World " + name);
     }
 }
