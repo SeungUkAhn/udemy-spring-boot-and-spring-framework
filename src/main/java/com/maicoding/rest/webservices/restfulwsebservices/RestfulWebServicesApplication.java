5df5053ca7ef8c2e136cd0a67668bf452b5acb97
SeungUk Ahn <ahn2802@gmail.com>
2023-07-13 23:19:50 +0900

    REST API 버전관리 - URI 이용

--- src/main/java/com/maicoding/rest/webservices/restfulwsebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/maicoding/rest/webservices/restfulwsebservices/RestfulWebServicesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestfulWsebServicesApplication {
+public class RestfulWebServicesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestfulWsebServicesApplication.class, args);
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
     }
 
 }
