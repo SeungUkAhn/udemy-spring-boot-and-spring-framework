6881eacd908191160d09143be54acba9da8f3342
SeungUk Ahn <ahn2802@gmail.com>
2023-07-18 03:06:52 +0900

    User: JPA 리팩토링
    - repository 생성, resource 리팩토링

--- src/main/java/com/maicoding/rest/webservices/restfulwsebservices/user/User.java
+++ src/main/java/com/maicoding/rest/webservices/restfulwsebservices/user/User.java
@@ -1,24 +1,37 @@
 package com.maicoding.rest.webservices.restfulwsebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+import javax.annotation.processing.Generated;
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
+
     @Size(min=2, message = "이름은 최소한 2자 이상이어야 합니다.")
-    @JsonProperty("user_name")
+    //@JsonProperty("user_name")
     private String name;
+
     @Past(message = "과거 날짜만 입력가능합니다.")
-    @JsonProperty("birth_date")
+    //@JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    protected User() {
+
     }
 
     public Integer getId() {
